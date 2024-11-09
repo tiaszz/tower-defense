d3467f6da1cb7a15bbd0036ceba8893b10071779
tiaszz <iskiizeh3@gmail.com>
2024-11-09 00:33:47 -0300

    "this dont work :)"

--- main.c
+++ main.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <conio.h>
+
+#define cols 20
+#define rows 20
+
+char board[cols * rows];
+
+void fill_board() {
+  int x,y;
+
+  for (y=0; y<rows; y++){
+    for (x=0; x<cols; x++){
+      if(x==0||y==0||x==cols-1||y==rows-1){
+        board[y*cols + x] = '#';
+      }
+    }
+  }
+}
+
+void print_board() {
+  int x,y;
+
+  for (y=0; y<rows; y++){
+    for (x=0; x<cols; x++){
+      putch(board[y*cols + x]);
+    }
+    putch('\n');
+  }
+}
 
 int main() {
-  return 0
+  fill_board();
+  print_board();
+
+
+  return 0;
 }
+
